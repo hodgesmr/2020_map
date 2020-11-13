8935fbef0d49c4c3531bf7978802239dd3c48eb9
Matt Hodges <hodgesmr1@gmail.com>
2020-11-12 23:32:40 -0700

    colors

--- build_map.py
+++ build_map.py
@@ -4,27 +4,73 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
 
-CWD = os.path.dirname(os.path.realpath(__file__))
-
-STATE_SHAPES_DIRECTORY = f'{CWD}/geographies'
-STATES_SHAPE_FILES = {state_file[:2]: f'{CWD}/geographies/{state_file}' for state_file in next(os.walk("geographies"))[2]}
-
 BLUE = "#1375B7"
 RED = "#C93135"
 WHITE = "#F2F4F8"
 
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+STATE_SHAPE_FILES = {state_file[:2]: f'{CWD}/geographies/{state_file}' for state_file in next(os.walk("geographies"))[2]}
+
+STATE_COLORS = {
+    'AK': RED,
+    'AL': RED,
+    'AR': RED,
+    'AZ': BLUE,
+    'CA': BLUE,
+    'CO': BLUE,
+    'CT': BLUE,
+    'DC': BLUE,
+    'DE': BLUE,
+    'FL': RED,
+    'GA': BLUE,
+    'HI': BLUE,
+    'IA': RED,
+    'ID': RED,
+    'IL': BLUE,
+    'IN': RED,
+    'KS': RED,
+    'KY': RED,
+    'LA': RED,
+    'MA': BLUE,
+    'MD': BLUE,
+    'ME': BLUE,
+    'MI': BLUE,
+    'MN': BLUE,
+    'MO': RED,
+    'MS': RED,
+    'MT': RED,
+    'NC': RED,
+    'ND': RED,
+    'NE': RED,
+    'NH': BLUE,
+    'NJ': BLUE,
+    'NM': BLUE,
+    'NV': BLUE,
+    'NY': BLUE,
+    'OH': RED,
+    'OK': RED,
+    'OR': BLUE,
+    'PA': BLUE,
+    'RI': BLUE,
+    'SC': RED,
+    'SD': RED,
+    'TN': RED,
+    'TX': RED,
+    'UT': RED,
+    'VA': BLUE,
+    'VT': BLUE,
+    'WA': BLUE,
+    'WI': BLUE,
+    'WV': RED,
+    'WY': RED,
+}
+
 fig, ax0 = plt.subplots(figsize=(20, 15))
 
-from random import randint
-
-for state_shape_file in STATES_SHAPE_FILES.values():
+for state, state_shape_file in STATE_SHAPE_FILES.items():
     state_df = gpd.read_file(state_shape_file)
     state_df = state_df.to_crs('esri:102003')
-    r = randint(0, 10)
-    if r%2:
-        color=RED
-    else:
-        color=BLUE
-    state_df.plot(ax=ax0, alpha=1, edgecolor=WHITE, color=color, linewidth=1)
+    state_df.plot(ax=ax0, alpha=1, edgecolor=WHITE, color=STATE_COLORS[state], linewidth=1)
 
 plt.show()
